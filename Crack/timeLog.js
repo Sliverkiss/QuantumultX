16893631746bfe18b1a8268441d375578fc92f66
Sliverkiss <1393579810@qq.com>
2024-06-26 20:05:05 +0800

    Update timeLog.js

--- Crack/timeLog.js
+++ Crack/timeLog.js
@@ -1,7 +1,6 @@
 /*
 时间日志 解锁永久会员 by chxm1024
-
-^http:\/\/liujia95\.xyz\/v1\/user\/get_user url script-response-body 
+^http:\/\/liujia95\.xyz\/v1\/user\/get_user url script-response-body https://raw.githubusercontent.com/Sliverkiss/QuantumultX/main/Crack/timeLog.js
 */
 
 $done({ body: JSON.stringify({ ...JSON.parse($response.body), body: { ...JSON.parse($response.body).body, vipType: 4 } }) });
