0d6c3f9de023852f924a5698ffdb8e388b5a7430
Sliverkiss <1393579810@qq.com>
2024-08-16 00:06:23 +0800

    Update xiaocan.js

--- AdBlock/xmApp/xiaocan.js
+++ AdBlock/xmApp/xiaocan.js
@@ -1,5 +1,5 @@
 let headers = JSON.parse($request.headers);
 if (headers?.methodname?.match(/GetBannerList/)) {
-    $done({});
+    $done(JSON.stringify({}));
 }
-$done();
+$done({ body: $response.body });
